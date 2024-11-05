798dc4bd6531075dee265983abcc6eae35001a39
hackartists <hackartists@gmail.com>
2024-11-05 14:08:04 +0900

    init

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+entry_point!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    Processor::process(program_id, accounts, instruction_data)
+}
